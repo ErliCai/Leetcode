d2c1e0fcae3bac14b36311eaaf2b2b09fb64fa9a
ErliCai <53851846+ErliCai@users.noreply.github.com>
2021-07-19 21:24:01 -0400

    rename

--- Leetcode_Problems/Leetcode001.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        
-        numsdic = {}
-        for i in range(len(nums)):
-            if nums[i] in numsdic:
-                return [i, numsdic[nums[i]]]
-            else:
-                numsdic[target-nums[i]] = i
